4676922d46aabf0cf144dec4364b3f104395180d
김종현 <jhkim@dit.ac.kr>
2017-09-02 19:11:36 +0900

    Initial Commit

---
+++ MultiPinMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiPinMap
+//
+//  Created by 김종현 on 2017. 9. 2..
+//  Copyright © 2017년 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
